0a925a96d7a32ee2ce89be3de401129101cf15ba
Miika Mäkelä <miika.makela@ssf.fi>
2020-02-22 11:46:57 +0200

    use absolute paths for the database

--- config/test_config.py
+++ config/test_config.py
@@ -1,8 +1,12 @@
+import os
+_project_root_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+_db_name = 'test.db'
+_db_path = os.path.join(_project_root_directory, _db_name)
 
-
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+DEBUG = True
+SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = f'sqlite:////{_db_path}'
 
-SECRECT_TOKEN_KEY = 'xQzgrlSycaaKMQI_Sm4OFYm8F6jIrgv0NnKJmVamPzxNoixf_sR1CCH799QdBRbadlbEr0fnq_Mx303JHV971XL_ckq4yKrVmrPnVuNssWE'
+SECRECT_TOKEN_KEY = 'suchsecrecymuchnumbers'
 SECRECT_TOKEN_EXPIRE_TIME = 500
