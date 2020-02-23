344bd25a0b3ee691c691d06257617b73e19d5d37
Miika Mäkelä <miika.makela@ssf.fi>
2020-02-23 15:57:17 +0200

    fix paths for database

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ _db_path = os.path.join(_project_root_directory, _db_name)
 DEBUG = False
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f'sqlite:////{_db_path}'
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{_db_path}'
 
 SECRECT_TOKEN_KEY = os.getenv('SECRET_TOKEN_KEY', 'suchsecrecymuchnumbers')
 SECRECT_TOKEN_EXPIRE_TIME = 500
